dc6c431dc49452339584dfb9d1299a4b1e4fd87c
tran-huu-tho <hyunjuntho@gmail.com>
2025-07-22 15:35:13 +0700

    bye

--- obj/Debug/net8.0/moon.AssemblyInfo.cs
+++ obj/Debug/net8.0/moon.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("moon")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+436a2691bb1e76f66f806c9a8ba670f624091d3e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c43634ad80751cc79f5dbd5717e17de33f837063")]
 [assembly: System.Reflection.AssemblyProductAttribute("moon")]
 [assembly: System.Reflection.AssemblyTitleAttribute("moon")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
